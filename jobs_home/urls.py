abef7b5d012d3719b3112e634114955ac3e4b5d8
Bewick <hanncus430@gmail.com>
2020-03-06 14:05:37 +0000

    Added placeholder tables to current and inbox. Current/inbox/archive now working off template base.html.

--- jobs_home/urls.py
+++ jobs_home/urls.py
@@ -7,7 +7,7 @@ app_name = 'jobs_home'
 urlpatterns = [
 
     path("", views.current, name='current'),
-    path("inbox", views.inbox, name="inbox"),
-    path("archive", views.archive, name="archive"),
+    path("inbox", views.inbox, name='inbox'),
+    path("archive", views.archive, name='archive'),
 
 ]
