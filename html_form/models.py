68d5a591e986e1cc062e663b097b435d43c339ac
Bewick <hanncus430@gmail.com>
2020-03-09 12:43:13 +0000

    Created models and migrated to db

--- html_form/models.py
+++ html_form/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Address(models.Model):
+    client = models.ManyToManyField('Client', related_name="client")
+    line_one = models.CharField(max_length=64)
+    line_two = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    county = models.CharField(max_length=64)
+    postcode = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{client.first} {client.last}\n{self.line_one}\n{self.line_two}\n{self.city}\n{self.county}\n{self.postcode}"
+
+
+
+class Client(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+    number = models.CharField(max_length=64)
+    address = models.ManyToManyField(Address, related_name="address")
+
+    def __str__(self):
+        return f"{self.first.capitalize} {self.last.capitalize}\n{self.email}\n{self.number}\n{self.address}"
+
+
+class Job(models.Model):
+    INBOX = 'IN'
+    CURRENT = 'CU'
+    ARCHIVE = 'AR'
+    Status_Choices = [
+        (INBOX, 'Inbox'),
+        (CURRENT, 'Current'),
+        (ARCHIVE, 'Archive'),
+    ]
+
+    creation_date = models.DateField
+    description = models.CharField(max_length=500)
+    status = models.CharField(max_length=2, choices=Status_Choices, default=INBOX)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return
--- html_form/views.py
+++ html_form/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import jobs_home.models
 
 def index(request):
-    return render(request, "html_form/index.html")
+    if request.method == "GET":
+        return render(request, "html_form/index.html")
+   
