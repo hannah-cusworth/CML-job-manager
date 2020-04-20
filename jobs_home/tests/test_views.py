e87c0cc438f4003b29ea23a3d1da634ce2d5166e
Bewick <hanncus430@gmail.com>
2020-04-20 13:05:29 +0100

    Improved detail view testing, now test multiple relationships.

--- jobs_home/tests/test_views.py
+++ jobs_home/tests/test_views.py
@@ -3,6 +3,7 @@ from jobs_home.views import *
 from django.contrib.auth.models import User
 from jobs_home.forms import *
 from jobs_home.filters import *
+from django.db.models import Q
 
 # Templates
 template_base = 'jobs_home/base.html'
@@ -12,7 +13,46 @@ template_client_card = 'jobs_home/client-card.html'
 template_address_card = 'jobs_home/address-card.html'
 template_detail = 'jobs_home/details.html'
 
+#by default transforms first item to a str using repr(). Overriden this with lambda so that it will literally compare the qs
+no_transform = lambda x:x
 
+class CreateData():
+
+    def create_object_set():
+        address = Address(line_one="foo", city="bar", county="Derby", postcode="baz")
+        person = Person(first="foo", last="bar", email="baz", number="123")
+        address.save()
+        person.save()
+        address.client.add(person.pk)
+        person.address.add(address.pk)
+        job = Job(description="foo", client=person, job_address=address, billing_address=address)
+        job.save()
+        return {"person": person, "job": job, "address": address}
+
+    def create_object_set_with_billing():
+        object_set = CreateData.create_object_set()
+        address_2 = Address(line_one="billing", city="billing", county="billing", postcode="billing", address_type="BILL")
+        address_2.save()
+        object_set["person"].address.add(address_2.pk)
+        address_2.client.add(object_set["person"].pk)
+        object_set["job"].billing_address = address_2
+        object_set["address_2"] = address_2 
+        return object_set
+
+    def create_object_set_multiple_clients():
+        object_set = CreateData.create_object_set()
+        person_2 = Person(first="person_2", last="person_2", email="person_2", number="person_2")
+        person_2.save()
+        person_2.address.add(object_set["address"].pk)
+        object_set["address"].client.add(person_2.pk)
+        object_set["person_2"] = person_2
+        return object_set
+
+    def create_object_set_multiple_jobs():
+        object_set = CreateData.create_object_set()
+        object_set["job_2"]  = Job(description="job_2", client=object_set["person"], job_address=object_set["address"], billing_address=object_set["address"])
+        object_set["job_2"].save()
+        return object_set
 
 
 class LoginViewTest(TestCase):
@@ -71,7 +111,7 @@ class ListViewTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-            create_object_set()
+            CreateData.create_object_set()
 
 
 class InboxViewTest(ListViewTest):
@@ -148,8 +188,10 @@ class ArchiveViewTest(TestCase):
     def test_archiveview_get_context(self):
         context = ArchiveView.get_context(self)
         self.assertIsInstance(context["job_search"], JobFilter)
-        self.assertEqual(context["address_search"], ClientFilter)
-        self.assertEqual(context["client_search"], AddressFilter)
+        self.assertEqual(context["address_search"], AddressFilter)
+        self.assertEqual(context["client_search"], ClientFilter)
+    
+    
 
 
 
@@ -161,23 +203,14 @@ class ArchiveViewTest(TestCase):
 
 dataset_size = 20
 
-def create_object_set():
-    address = Address(line_one="foo", city="bar", county="Derby", postcode="baz")
-    person = Person(first="foo", last="bar", email="baz", number="123")
-    address.save()
-    person.save()
-    address.client.add(person.pk)
-    person.address.add(address.pk)
-    job = Job(description="foo", client=person, job_address=address, billing_address=address)
-    job.save()
+
+
 
 class DetailViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         for i in range(0 ,dataset_size):
-            create_object_set()
-        
-        
+            CreateData.create_object_set()
        
     def test_data(self):
         self.assertEqual(Job.objects.all().count(), dataset_size)
@@ -234,8 +267,7 @@ class JobViewTest(DetailViewTest):
         response = self.client.post(self.view, data={'description': new_text})
         self.job.refresh_from_db()          #NB you have to refresh, v imp
         self.assertEqual(self.job.description, new_text)
-        
-    
+         
 
 class ClientViewTest(DetailViewTest):
     def setUp(self):
@@ -257,7 +289,6 @@ class ClientViewTest(DetailViewTest):
         self.assertEqual(response.status_code, 200)
         self.check_templates(response)
         
-
     def test_clientview_404(self):
         response = self.client.get(self.not_found)
         self.assertEqual(response.status_code, 404)
@@ -265,9 +296,21 @@ class ClientViewTest(DetailViewTest):
     def test_clientview_context(self):
         response = self.client.get(self.view)
         self.assertEqual(response.context['client'], self.person)
-        self.assertQuerysetEqual(response.context['related'].order_by('id'), self.person.address.all().order_by('id'), transform= lambda x:x)
-        self.assertQuerysetEqual(response.context['jobs'].order_by('id'), Job.objects.filter(client_id = self.person.pk).order_by('id'), transform= lambda x: x)
+        self.assertQuerysetEqual(response.context['related'].order_by('id'), self.person.address.all().order_by('id'), transform=no_transform)
+        self.assertQuerysetEqual(response.context['jobs'].order_by('id'), Job.objects.filter(client_id = self.person.pk).order_by('id'), transform=no_transform)
 
+    def test_clientview_context_2_jobs(self):
+        object_set = CreateData.create_object_set_multiple_jobs()
+        client_id = object_set["person"].pk
+        response = self.client.get("/client/" + str(client_id))
+        self.assertQuerysetEqual(response.context['jobs'].order_by('pk'), Job.objects.filter(client_id=client_id).order_by('pk'), transform=no_transform)
+    
+    def test_clientview_context_with_billing(self):
+        object_set = CreateData.create_object_set_with_billing()
+        client_id = object_set["person"].pk
+        response = self.client.get("/client/" + str(client_id))
+        self.assertQuerysetEqual(response.context['related'].order_by('pk'), Address.objects.filter(client=client_id).order_by('pk'), transform=no_transform)
+    
     def test_clientview_post_status(self):
         response = self.client.post(self.view)
         self.assertEqual(response.status_code, 200)
@@ -306,9 +349,20 @@ class AddressViewTest(DetailViewTest):
     def test_addressview_context(self):
         response = self.client.get(self.view)
         self.assertEqual(response.context['address'], self.address)
-        #by default transforms first item to a str using repr(). Overriden this with lambda so that it will literally compare the qs
-        self.assertQuerysetEqual(response.context['related'], self.address.client.all().order_by('pk'), transform=lambda x: x)
-        self.assertQuerysetEqual(response.context['jobs'].order_by('pk'), Job.objects.filter(job_address=self.address_id).order_by('pk'), transform=lambda x:x)
+        self.assertQuerysetEqual(response.context['related'], self.address.client.all().order_by('pk'), transform=no_transform)
+        self.assertQuerysetEqual(response.context['jobs'].order_by('pk'), Job.objects.filter(Q(job_address_id=self.address_id)|Q(billing_address_id=self.address_id)).order_by('pk'), transform=no_transform)
+    
+    def test_addressview_context_2_jobs(self):
+        object_set = CreateData.create_object_set_multiple_jobs()
+        address_id = object_set["address"].pk
+        response = self.client.get("/address/" + str(address_id))
+        self.assertQuerysetEqual(response.context['jobs'].order_by('pk'), Job.objects.filter(Q(job_address_id=address_id)|Q(billing_address_id=address_id)).order_by('pk'), transform=no_transform)
+
+    def test_addressview_context_2_clients(self):
+        object_set = CreateData.create_object_set_multiple_clients()
+        address = object_set["address"]
+        response = self.client.get("/address/" + str(address.pk))
+        self.assertQuerysetEqual(response.context['related'].order_by('pk'), address.client.all().order_by('pk'), transform=no_transform )
     
     def test_addressview_post_status(self):
         response = self.client.post(self.view)
